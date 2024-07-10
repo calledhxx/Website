788130ae67e856486de97084bdd5e1207532d6d1
Hxx <160889760+calledhxx@users.noreply.github.com>
2024-07-10 15:22:33 +0800

    Add files via upload

--- header.js
+++ header.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Hello");
     const Div = document.createElement("div");
-    const Title = document.createElement("h4");
+    const Link = document.createElement("a");
+    const C1 = document.createElement("h4");
+    const C2 = document.createElement("h4");
     const MsgBox = document.createElement("div");
     Div.id = "lit-msg";
     Div.style = ""
@@ -21,12 +23,23 @@ document.addEventListener("DOMContentLoaded", function() {
     +"display: flex;\n" +
         "    justify-content: center;\n" +
         "    align-items: center;"
-
-    Title.textContent = "Use \"UD Digi Kyokasho N-B\" to make everything pretty."
-    Title.style = ""
+    Link.href = "https://fonts.adobe.com/fonts/uddigikyokasho-pro"
+    Link.textContent = "UD Digi Kyokasho N-B"
+    C1.textContent = " Use"
+    C1.style = ""
         +"color : rgb(100,100,100);"
     +"font-size:0.1em;"+" text-shadow: black 0.1em 0.1em 1em"
+    Link.style = ""
+        +"color : rgb(150,150,150);"
+        +"font-size:0.1em;"+" text-shadow: black 0.1em 0.1em 1em"
+    C2.textContent = "to make everything prettier."
+    C2.style =  ""
+        +"color : rgb(100,100,100);"
+        +"font-size:0.1em;"+" text-shadow: black 0.1em 0.1em 1em"
+
     document.getElementById("body").appendChild(Div);
-    MsgBox.appendChild(Title);
+    MsgBox.appendChild(C1);
+    MsgBox.appendChild(Link);
+    MsgBox.appendChild(C2);
     Div.appendChild(MsgBox);
 })
